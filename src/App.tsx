64efaaa85d663c8d08133479484b9dab24ff1f8c
jp.secundino <jp.secundino@usp.br>
2023-01-29 16:31:49 -0300

    Add search product functionality.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
+import TextField from '@mui/material/TextField';
 import './App.css';
-import axios, { AxiosResponse } from 'axios';
 import ProductCard from './components/ProductCard';
 import ProductFetcher from './components/ProductFetcher';
 
@@ -10,16 +10,20 @@ function App() {
     let [products, setProducts] = useState([]);
     let [searchTerm, setSearchTerm] = useState('')
 
-    useEffect(() => {
-        getProducts(searchTerm);
-    }, []);
-
     const getProducts = (searchTerm:string) => {
         ProductFetcher.getProducts(searchTerm)
-                      .then(response => setProducts(response.data))
+            .then(response => setProducts(response.data));
     }
+
     return (
         <div className="App">
+            <form onSubmit={(event) => event.preventDefault()}>
+            <label>
+                Product name:
+                    <input type="text" name="name" value={searchTerm} onChange={(event) => { setSearchTerm(event.target.value); console.log(searchTerm)}} />
+                </label>
+                <input type="submit" value="Submit" onClick={() => getProducts(searchTerm)} />
+            </form>
             {products.map(product =>
                 <ProductCard
                     name={product['name']}
